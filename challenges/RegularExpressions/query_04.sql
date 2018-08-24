b4ee4ccde62cba5b6b114a71f7c4956abbc2d9e7
harsha547 <harsha.sannareddy@yahoo.com>
2018-08-25 00:00:33 +0530

    IN Operator

---
+++ challenges/RegularExpressions/query_04.sql
@@ -0,0 +1,6 @@
+-- 4. What are the products with a product code in the range S700_1000 to S700_1499?
+
+SELECT productCode,productName
+FROM Products
+WHERE RIGHT(productCode,4) BETWEEN 1000 AND 1499
+ORDER BY RIGHT(productCode,4)
