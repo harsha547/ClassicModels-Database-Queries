5144e77fc2bf4ab2c053db4c5f9948f1b76a62d9
harsha547 <harsha.sannareddy@yahoo.com>
2018-08-24 23:46:33 +0530

    Amount paid by each customer

--- challenges/OnetoMany_Relationship/query_05.sql
+++ challenges/OnetoMany_Relationship/query_05.sql
@@ -1,1 +1,10 @@
 -- 5.List the amount paid by each customer.
+
+SELECT Orders.customerNumber, customerName , ROUND(SUM(Detail.quantityOrdered*Detail.priceEach),2) As 'Amount Paid'
+FROM Customers
+INNER JOIN Orders
+ON Customers.customerNumber = Orders.customerNumber
+INNER JOIN OrderDetails Detail
+on Orders.orderNumber = Detail.orderNumber
+GROUP BY Orders.customerNumber,customerName
+ORDER BY SUM(Detail.quantityOrdered*Detail.priceEach) DESC
